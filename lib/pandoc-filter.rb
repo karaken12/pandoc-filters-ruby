32391c121b5c78244bc48aaa5c94709017990a86
Mike Virata-Stone <mjstone@on-site.com>
2016-08-04 17:58:05 -0700

    Include more data types

--- lib/pandoc-filter.rb
+++ lib/pandoc-filter.rb
@@ -74,7 +74,7 @@ module PandocElement
     elsif object.kind_of?(Hash) && object.include?('t') && object.include?('c')
       raise "Unknown type: #{object['t']}" unless PandocElement.const_defined?(object['t'])
       type = PandocElement.const_get(object['t'])
-      raise "Invalid type: #{object['t']}" unless type < PandocElement::Base
+      raise "Invalid type: #{object['t']}" unless type < PandocElement::BaseElement
       type.new(to_object(object['c']))
     elsif object.kind_of?(Hash)
       result = {}
@@ -88,12 +88,16 @@ module PandocElement
   class Base
     attr_reader :contents
 
+    def self.contents_attr(name, index)
+      define_method(name) { contents[index] }
+    end
+
     def initialize(contents = [])
       @contents = contents
     end
 
     def to_ast
-      { 't' => element_name, 'c' => PandocElement.to_ast(contents) }
+      PandocElement.to_ast(contents)
     end
 
     def ==(other)
@@ -101,40 +105,83 @@ module PandocElement
     end
   end
 
-  [ ['Plain', :elements],
-    ['Para', :elements],
-    ['CodeBlock', :attributes, :value],
-    ['RawBlock', :format, :value],
-    ['BlockQuote', :elements],
-    ['OrderedList', :attributes, :elements],
-    ['BulletList', :elements],
-    ['DefinitionList', :elements],
-    ['Header', :level, :attributes, :elements],
-    ['HorizontalRule'],
-    ['Table', :captions, :alignments, :widths, :headers, :rows],
-    ['Div', :attributes, :elements],
-    ['Null'],
-    ['Str', :value],
-    ['Emph', :elements],
-    ['Strong', :elements],
-    ['Strikeout', :elements],
-    ['Superscript', :elements],
-    ['Subscript', :elements],
-    ['SmallCaps', :elements],
-    ['Quoted', :type, :elements],
-    ['Cite', :citations, :elements],
-    ['Code', :attributes, :value],
-    ['Space'],
-    ['SoftBreak'],
-    ['LineBreak'],
-    ['Math', :type, :value],
-    ['RawInline', :format, :value],
-    ['Link', :attributes, :elements, :target],
-    ['Image', :attributes, :elements, :target],
-    ['Note', :elements],
-    ['Span', :attributes, :elements]
+  class BaseElement < PandocElement::Base
+    def to_ast
+      { 't' => element_name, 'c' => PandocElement.to_ast(contents) }
+    end
+  end
+
+  module Inline
+  end
+
+  module Block
+  end
+
+  class Attr < PandocElement::Base
+    contents_attr :identifier, 0
+    contents_attr :classes, 1
+    contents_attr :key_values, 2
+
+    def [](key)
+      key_values_hash[key]
+    end
+
+    def include?(key)
+      key_values_hash.include?(key)
+    end
+
+    private
+
+    def key_values_hash
+      @key_values_hash ||= Hash[key_values]
+    end
+  end
+
+  class Target < PandocElement::Base
+    contents_attr :url, 0
+    contents_attr :title, 1
+  end
+
+  [ ['Plain',          :elements,                                        { include: [PandocElement::Block] }],
+    ['Para',           :elements,                                        { include: [PandocElement::Block] }],
+    ['CodeBlock',      :attributes, :value,                              { include: [PandocElement::Block] }],
+    ['RawBlock',       :format, :value,                                  { include: [PandocElement::Block] }],
+    ['BlockQuote',     :elements,                                        { include: [PandocElement::Block] }],
+    ['OrderedList',    :attributes, :elements,                           { include: [PandocElement::Block] }],
+    ['BulletList',     :elements,                                        { include: [PandocElement::Block] }],
+    ['DefinitionList', :elements,                                        { include: [PandocElement::Block] }],
+    ['Header',         :level, :attributes, :elements,                   { include: [PandocElement::Block] }],
+    ['HorizontalRule',                                                   { include: [PandocElement::Block] }],
+    ['Table',          :captions, :alignments, :widths, :headers, :rows, { include: [PandocElement::Block] }],
+    ['Div',            :attributes, :elements,                           { include: [PandocElement::Block] }],
+    ['Null',                                                             { include: [PandocElement::Block] }],
+    ['Str',            :value,                                           { include: [PandocElement::Inline] }],
+    ['Emph',           :elements,                                        { include: [PandocElement::Inline] }],
+    ['Strong',         :elements,                                        { include: [PandocElement::Inline] }],
+    ['Strikeout',      :elements,                                        { include: [PandocElement::Inline] }],
+    ['Superscript',    :elements,                                        { include: [PandocElement::Inline] }],
+    ['Subscript',      :elements,                                        { include: [PandocElement::Inline] }],
+    ['SmallCaps',      :elements,                                        { include: [PandocElement::Inline] }],
+    ['Quoted',         :type, :elements,                                 { include: [PandocElement::Inline] }],
+    ['Cite',           :citations, :elements,                            { include: [PandocElement::Inline] }],
+    ['Code',           :attributes, :value,                              { include: [PandocElement::Inline] }],
+    ['Space',                                                            { include: [PandocElement::Inline] }],
+    ['SoftBreak',                                                        { include: [PandocElement::Inline] }],
+    ['LineBreak',                                                        { include: [PandocElement::Inline] }],
+    ['Math',           :type, :value,                                    { include: [PandocElement::Inline] }],
+    ['RawInline',      :format, :value,                                  { include: [PandocElement::Inline] }],
+    ['Link',           :attributes, :elements, :target,                  { include: [PandocElement::Inline] }],
+    ['Image',          :attributes, :elements, :target,                  { include: [PandocElement::Inline] }],
+    ['Note',           :elements,                                        { include: [PandocElement::Inline] }],
+    ['Span',           :attributes, :elements,                           { include: [PandocElement::Inline] }]
   ].each do |name, *params|
     name.freeze
+
+    options = if params.last.kind_of?(Hash)
+      params.pop
+    else
+      {}
+    end
 
     case params.size
     when 0
@@ -153,11 +200,13 @@ module PandocElement
       raise "Too many parameters!"
     end
 
-    const_set(name, Class.new(PandocElement::Base) {
+    const_set(name, Class.new(PandocElement::BaseElement) {
+      (options[:include] || []).each { |mod| include mod }
+
       if params.size == 1
         define_method(params.first) { contents }
       else
-        params.each_with_index { |param, index| define_method(param) { contents[index] } }
+        params.each_with_index { |param, index| contents_attr param, index }
       end
 
       define_method(:element_name) { name }
--- test/elements_test.rb
+++ test/elements_test.rb
@@ -4,12 +4,14 @@ class ElementsTest < Minitest::Test
   def test_space
     space = PandocElement::Space.new
     assert_equal([], space.contents)
+    assert space.kind_of?(PandocElement::Inline)
   end
 
   def test_str
     str = PandocElement::Str.new('hello')
     assert_equal('hello', str.contents)
     assert_equal('hello', str.value)
+    assert str.kind_of?(PandocElement::Inline)
   end
 
   def test_para
@@ -22,5 +24,27 @@ class ElementsTest < Minitest::Test
     para = PandocElement::Para.new(elements)
     assert_equal(elements, para.contents)
     assert_equal(elements, para.elements)
+    assert para.kind_of?(PandocElement::Block)
+  end
+
+  def test_link
+    link = PandocElement::Link.new([
+      PandocElement::Attr.new(['id', ['class1', 'class2'], [['key1', 'value1'], ['key2', 'value2']]]),
+      [PandocElement::Str.new('link')],
+      PandocElement::Target.new(['http://example.com', 'This is the title'])
+    ])
+
+    assert_equal('id', link.attributes.identifier)
+    assert_equal(['class1', 'class2'], link.attributes.classes)
+    assert_equal([['key1', 'value1'], ['key2', 'value2']], link.attributes.key_values)
+    assert_equal('value1', link.attributes['key1'])
+    assert_equal('value2', link.attributes['key2'])
+    assert_equal(nil, link.attributes['key3'])
+    assert_equal(true, link.attributes.include?('key1'))
+    assert_equal(true, link.attributes.include?('key2'))
+    assert_equal(false, link.attributes.include?('key3'))
+    assert_equal([PandocElement::Str.new('link')], link.elements)
+    assert_equal('http://example.com', link.target.url)
+    assert_equal('This is the title', link.target.title)
   end
 end
